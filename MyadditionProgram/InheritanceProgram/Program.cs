83b0f4b757a75e3dd2cf7d397574b1af8c865430
Monu Kumar <99990223+MonuKumar8445@users.noreply.github.com>
2022-03-07 13:42:22 +0530

    Add files via upload

---
+++ MyadditionProgram/InheritanceProgram/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Inheritance
+{
+    class Car
+    {
+        public string model;
+        public string color;
+        public int price;
+
+        public void CarInformation()
+        {
+            Console.WriteLine("Model:" + model);
+            Console.WriteLine("Color:" + color);
+            Console.WriteLine("Price" + price);
+
+        }
+       
+    }
+    class Maruti:Car
+    {
+        float mileage;
+        public void Mileage()
+        {
+          Console.WriteLine("Mileage:" + mileage);
+        }
+    
+        static void Main(string[] args)
+        {
+            Maruti myCar = new Maruti();
+            myCar.model="Ford";
+            myCar.color="Blue" ;
+            myCar.price = 4999990;
+            myCar.mileage = 12.5f;
+            myCar.CarInformation();
+            myCar.Mileage();
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
