dbcf4256757247f5af38ef84cb0b3a69ffa48f21
Monu Kumar <99990223+MonuKumar8445@users.noreply.github.com>
2022-03-07 13:50:01 +0530

    Add files via upload

---
+++ Myprogramstarprint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Myprogramstarpattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count=0;
+            for (int i =1 ; i <=7; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    count++;
+                    Console.Write(count);
+                }
+                Console.WriteLine();    
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
